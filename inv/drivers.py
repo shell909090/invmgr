08344417dd7ec090045ecebcbcd3c1b077f3ce4c
Shell.Xu <shell909090@gmail.com>
2021-06-22 10:57:49 +0800

    upgrade investing
    add driver: SinaFin

--- inv/drivers.py
+++ inv/drivers.py
@@ -9,6 +9,7 @@
 from __future__ import unicode_literals
 import json
 
+from django.conf import settings
 import requests
 import investpy
 from bs4 import BeautifulSoup
@@ -39,6 +40,13 @@ def EastmoneyFund(_id):
     doc = BeautifulSoup(resp.content, 'lxml')
     for span in doc.select('span.fix_dwjz'):
         return span.get_text()
+
+
+def SinaFin(_id):
+    # 股票名称、今日开盘价、昨日收盘价、当前价格、今日最高价、今日最低价、竞买价、竞卖价
+    # 成交股数、成交金额、买1手、买1报价、买2手、买2报价、…、买5报价、…、卖5报价、日期、时间
+    resp = requests.get(f'http://hq.sinajs.cn/list={_id}', proxies=no_proxies)
+    return resp.text.split('"')[1].split(',')[3]
 
 
 def InvestingFund(_id):
